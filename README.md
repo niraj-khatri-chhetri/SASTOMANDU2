8f28c93001ded7586bc21e8cb1436fcab8ef770e
Niraj Kc <nirajkc98@gmail.com>
2023-05-12 21:44:13 +0545

    add the project

---
+++ README.md
@@ -0,0 +1,42 @@
+# MERN E-COMMERCE TUTORIAL
+
+Hi! My name is **Niraj Kc**, I have created this project to sell and buy second hand products online using MERN Stack for final year project.
+
+# Prerequisite
+
+1.Knowledge of **Node**, **React**, **Express**, **MongoDB** .
+
+# Install Dependencies
+
+**For Backend** - `npm i`
+
+**For Frontend** - `cd frontend` ` npm i`
+
+## Env Variables
+
+Make Sure to Create a config.env file in backend/config directory and add appropriate variables in order to use the app.
+
+**Essential Variables**
+PORT=
+DB*URI =
+STRIPE_API_KEY=
+STRIPE_SECRET_KEY=
+JWT_SECRET=
+JWT_EXPIRE=
+COOKIE_EXPIRE=
+SMPT_SERVICE =
+SMPT_MAIL=
+SMPT_PASSWORD=
+SMPT_HOST=
+SMPT_PORT=
+CLOUDINARY_NAME
+CLOUDINARY_API_KEY
+CLOUDINARY_API_SECRET
+\_fill each filed with your info respectively*
+
+## Author
+
+**Instagram** Click [Here](https://www.instagram.com/meabhisingh) **@meAbhiSingh**
+**YouTube** Click [Here](https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw/) **6 Pack Programmer**
+**LinkedIn** Click [Here](https://in.linkedin.com/in/meabhisingh) **@meAbhiSingh**
+**Twitter** Click [Here](https://twitter.com/meAbhi_Singh) **@meAbhi_Singh**
---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './aboutSection.css';
+import { Button, Typography, Avatar } from '@material-ui/core';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import InstagramIcon from '@material-ui/icons/Instagram';
+const About = () => {
+  const visitInstagram = () => {
+    window.location = 'https://instagram.com';
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: '10vmax', height: '10vmax', margin: '2vmax 0' }}
+              src=""
+              alt="Founder"
+            />
+            <Typography>
+              <ul>Niraj Sudip Dipendra</ul>
+            </Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This web application is made as a final year project by the group
+              that includes following memebers: Niraj Kc,Dipendra Pahadi,Sudip
+              Adhikari.The project is completed on top of the latest
+              technologies and practices such as
+              mongodb,react.js,node.js,express.js and so on.
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a href="https://www.youtube.com" target="blank">
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
